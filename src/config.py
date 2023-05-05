37706d5988814ba59e7678c9b05595e0c4a08d5e
Charlie Marshall <charlie.marshall1996@gmail.com>
2023-05-05 14:57:31 +0100

    init

---
+++ src/config.py
@@ -0,0 +1,9 @@
+# Configuration constants
+
+DEBUG = False
+LOGGING_LEVEL = 'debug'
+DATA_DIR = 'data'
+CACHE_DIR = 'cache'
+DEFAULT_TIMEOUT = 60
+MAX_RETRIES = 5
+RETRY_DELAY = 2
---
+++ src/scripts/backoff.py
@@ -0,0 +1,42 @@
+import time
+
+
+def exponential_backoff(func, max_retries=5, delay=1, backoff=2):
+    """
+    Executes a function with exponential backoff and retry if an exception occurs.
+
+    Args:
+        func (callable): The function to execute.
+        max_retries (int): The maximum number of retries.
+        delay (float): The initial delay in seconds between retries.
+        backoff (float): The backoff factor applied to the delay after each retry.
+
+    Returns:
+        The result of the function if it is successful, otherwise raises the last caught exception.
+    """
+
+    # Initialise attempts counter
+    attempts = 0
+
+    # While the attempts is lessthan/equal to maximum number of defined tries
+    while attempts <= max_retries:
+
+        # attempt to return the functions value
+        try:
+            return func()
+
+        # if there is an exception
+        except Exception as e:
+
+            # and the number of attempts is the same as the max number of tries
+            if attempts == max_retries:
+
+                # raise an exception
+                raise e
+
+            # otherwise, add one to the attempts counter
+            # square the delay time
+            else:
+                attempts += 1
+                time.sleep(delay)
+                delay *= backoff
