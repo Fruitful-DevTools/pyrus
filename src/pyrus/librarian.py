c3e34818b5bb96f7bbed6906d584077fe25c7c1e
Charlie Marshall <charlie.marshall1996@gmail.com>
2023-05-31 22:29:22 +0100

    changes

--- src/pyrus/librarian.py
+++ src/pyrus/librarian.py
@@ -1,14 +1,15 @@
 from contextlib import contextmanager
 from collections import ChainMap
-from pandas import read_csv
 import csv
+from typing import Dict, List, Tuple, TextIO
 
 
 class Librarian:
+    
     LIBRARY = 'library/'
 
-    def __init__(self, custom_library):
-        self.custom_library = custom_library
+    def __init__(self):
+        pass
 
     @contextmanager
     def get_collection(self, collection):
@@ -16,7 +17,7 @@ class Librarian:
         file = None
 
         try:
-            file = open(collection, 'r', newline='')
+            file = open(collection, 'r', newline='', encoding='utf-8-sig')
             yield file
 
         except Exception as e:
@@ -31,12 +32,37 @@ class Librarian:
         collection_filepath = f'{self.LIBRARY}{collection}.csv'
 
         with self.get_collection(collection_filepath) as file:
-            result_dict = csv.DictReader(file)
-            print(result_dict)
-            result_map = ChainMap(result_dict)
-
+            open_file = csv.reader(file)
+            unmapped_dicts = self.extrapolate_dicts(open_file)
+            result_map = self.create_datamap(*unmapped_dicts)
+            print(result_map)
             for key in keys:
                 if key in result_map:
-                    result_file = result_map[key]
+                    result_map = result_map[key]
 
-            return result_file
+            return result_map
+    
+    def extrapolate_dicts(self, open_file: TextIO) -> Tuple[Dict[str, Dict[str, str]], Dict[str, str], List[List[str]], List[str]]:
+        
+        rows = list(open_file)
+        parent_keys = rows[0]
+        child_dict = {}
+
+        for row in rows[1:]:
+            child_key = row[0]
+            child_value = row[1]
+            child_dict[child_key] = child_value
+
+        parent_dict = dict(zip(parent_keys, [child_dict] * len(parent_keys)))
+        return parent_dict, child_dict, rows, parent_keys
+        
+    def create_datamap(self, parent_dict: dict, child_dict: dict, rows: list, parent_keys: tuple) -> ChainMap:
+
+        for row in rows[1:]:
+            child_key = row[0]
+            child_value = row[1]
+            child_dict[child_key] = child_value
+
+        parent_dict = dict(zip(parent_keys, [child_dict] * len(parent_keys)))
+        data_map = ChainMap(parent_dict, child_dict)
+        return data_map
